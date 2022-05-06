7e462a0e70bb5a1f6b14244db455999e1792e3e1
Subhash <subhash.morla@gmail.com>
2022-05-06 17:10:33 +0530

    swipe down added

--- my-components/components/swipe-down/index.js
+++ my-components/components/swipe-down/index.js
@@ -1,14 +1,47 @@
-import React from 'react'
-import { View,Text } from 'react-native'
+import React, { useEffect } from "react";
+import { Dimensions, StyleSheet } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
+
+const {height} = Dimensions.get("window")
 
 function SwipeDown() {
+  const position = useSharedValue(0);
+
+  useEffect(() => {
+    position.value=0
+
+  }, [])
+  
+
+  const panGesture = Gesture.Pan()
+    .onUpdate((e) => {
+        position.value =  e.translationY;
+    })
+    .onEnd((e) => {
+      if (position.value > 200) {
+        position.value = withTiming(height, { duration: 100 });
+      } else {
+        position.value = withTiming(0, { duration: 100 });
+      }
+    });
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateY: position.value , scale: inter}],
+  }));
   return (
-    <View>
-        <Text>
-            Test
-        </Text>
-    </View>
-  )
+    <GestureDetector gesture={panGesture}>
+      <Animated.View style={[styles.box, animatedStyle]} />
+    </GestureDetector>
+  );
 }
 
-export default SwipeDown
+const styles= StyleSheet.create({
+    box: {
+        width:300,
+        height:300,
+        backgroundColor: 'red'
+    }
+})
+
+export default SwipeDown;
