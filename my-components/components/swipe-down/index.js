1c2dcd1d5c9efd7d6359e574edcc9c539bf779e8
Subhash <subhash.morla@gmail.com>
2022-05-06 13:59:06 +0530

    first commit my components

---
+++ my-components/components/swipe-down/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View,Text } from 'react-native'
+
+function SwipeDown() {
+  return (
+    <View>
+        <Text>
+            Test
+        </Text>
+    </View>
+  )
+}
+
+export default SwipeDown
